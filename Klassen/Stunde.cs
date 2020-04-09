b22f1739b2d4188252fdb46d5fc27dd8d2d7a583
klu5 <63228036+klu5@users.noreply.github.com>
2020-04-09 12:03:38 +0200

    Klasse Fach hinzugefügt & Stunde.Color als Brush (#5)
    Co-authored-by: Lübbesmeier, Kai <klu@connext.de>

--- Klassen/Stunde.cs
+++ Klassen/Stunde.cs
@@ -2,16 +2,12 @@
 {
     public class Stunde
     {
-        public string Titel { get; set; }
-        public string Color { get; set; }
-        public string Lehrer { get; set; }
+        public Fach Fach { get; set; }
         public string Raum { get; set; }
 
-        public Stunde(string titel, string color, string lehrer, string raum)
+        public Stunde(Fach fach, string raum)
         {
-            Titel = titel;
-            Color = color;
-            Lehrer = lehrer;
+            Fach = fach;
             Raum = raum;
         }
     }
